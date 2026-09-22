3d7b45e8b3751dcb1043168bccff4c73be248381
alpincitayem <alfil269298@student.smkn1kandeman.sch.id>
2026-09-22 09:02:20 +0700

    Add files via upload

---
+++ Kondisi_Pilihan/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kondisi_Pilihan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nama : M Alfil David Rizal Jiban
+            // Kelas : X PPLG 2
+            // Kondisi dengan satu pilihan
+            // Jika nilai >= 75 maka lulus
+            // Variabel nilai int
+
+            int nilai = 95;
+            if (nilai >= 95)
+            {
+                // pilihan 1
+                Console.WriteLine("MASHAALLAH");
+            }
+            else if (nilai >= 75)
+            {
+                // pilihan 2
+                Console.WriteLine("SELAMAT ANDA LULUS");
+            }
+            else
+            {
+                Console.WriteLine("SUBHANNALLAH");
+            }
+
+            }
+        }
+}
