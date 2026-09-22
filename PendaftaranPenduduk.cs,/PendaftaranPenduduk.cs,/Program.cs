d96676ef96115d1e6b20367d0e9232e277793989
alpincitayem <alfil269298@student.smkn1kandeman.sch.id>
2026-09-22 07:54:21 +0700

    Add files via upload

---
+++ PendaftaranPenduduk.cs,/PendaftaranPenduduk.cs,/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PendaftaranPenduduk.cs_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat variabel kosong
+            string nama;
+            int umur;
+
+            Console.WriteLine("=== PROGRAM PENDAFTARAN PENDUDUK ===");
+            Console.WriteLine("Masukan nama: ");
+            nama = Console.ReadLine();
+            Console.Write("Masukan alamat: ");
+            var alamat = Console.ReadLine();
+            Console.Write("Masukan umur: ");
+            umur = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Terima kasih!");
+            Console.WriteLine("Data berikut");
+            Console.WriteLine($"Nama: {nama}");
+            Console.WriteLine($"Alamat: {alamat}");
+            Console.WriteLine($"Umur: {umur} tahun");
+            Console.WriteLine("SUDAH DISIMPAN!");
+
+
+        }
+    }
+}
