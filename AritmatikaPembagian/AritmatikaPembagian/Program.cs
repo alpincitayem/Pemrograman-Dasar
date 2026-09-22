3c6d428dad42df6828863e2c8c408e8c72f958df
alpincitayem <alfil269298@student.smkn1kandeman.sch.id>
2026-09-22 07:30:43 +0700

    Add files via upload

---
+++ AritmatikaPembagian/AritmatikaPembagian/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmatikaPembagian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mangga, orang, hasil = 0;
+
+            Console.WriteLine("jumblah mangga = ");
+            mangga = int.Parse(Console.ReadLine());
+            Console.WriteLine("jumblah orang = ");
+            orang = int.Parse(Console.ReadLine());
+
+            hasil = mangga % orang;
+
+            Console.WriteLine($"Hasil mangga % orang = {hasil}");
+        }
+    }
+}
---
+++ AritmatikaPengurangan/AritmatikaPengurangan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AritmatikaPengurangan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mangga, apel, hasil = 0;
+
+            Console.Write("mangga = ");
+            mangga = int.Parse(Console.ReadLine());
+            Console.Write("appel = ");
+            apel = int.Parse(Console.ReadLine());
+
+            hasil = mangga - apel;
+
+            Console.WriteLine($"Hasil mangga - appel = {hasil}");
+        }
+    }
+}
